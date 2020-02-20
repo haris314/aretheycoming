7ef8b2facbd6c17bf527c3154717988c289a998a
Mohd Haris <hrs.haris.hrs@gmail.com>
2020-02-20 21:59:21 +0530

    Some deployment stuff

--- aretheycoming/settings.py
+++ aretheycoming/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for aretheycoming project.
 
@@ -142,3 +143,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
