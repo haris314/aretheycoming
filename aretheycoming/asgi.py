a40031b2ff2b4c891483bc8d20d0a51b0cf9d3e9
Mohd Haris <hrs.haris.hrs@gmail.com>
2020-02-20 23:15:21 +0530

    Got online in the previous commit but database connections exceeding limit

--- aretheycoming/asgi.py
+++ aretheycoming/asgi.py
@@ -4,4 +4,5 @@ from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aretheycoming.settings")
 django.setup()
+os.environ['ASGI_THREADS']="15"
 application = get_default_application()
