d9a9ea7c1eeed2b0830607abb7de63d6484a6b46
Mohd Haris <hrs.haris.hrs@gmail.com>
2020-02-21 18:34:42 +0530

    Added everything

--- aretheycoming/settings.py
+++ aretheycoming/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'home',
     'users',
     'groups',
@@ -73,6 +74,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aretheycoming.wsgi.application'
 ASGI_APPLICATION = 'aretheycoming.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
+        },
+    },
+}
 
 
 # Database
